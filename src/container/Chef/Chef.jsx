d59074b3dcf96b40747d8ff996820ceb2f55904c
Alish Shrestha <alish@Alishs-MacBook-Air.local>
2023-08-28 18:32:45 +0530

    initial commit

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
+
+const Chef = () => (
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt='chef' />
+    </div>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word" />
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt='quote' />
+          <p className='p__opensans'>
+            Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit Auctor Sit.
+          </p>
+        </div>
+
+        <p className='p__opensans'>
+          Auctor Sit laculis In Arcu. Vulputate Nulla Lobortis Mauris Eget Sit.
+          Nulla Scelerisque Scelerisque Congue Ac Consequat. Aliquam Molestie
+          Letctus Eu. Congue iaculls Integer Curabitur Semper Sit Nunt.
+        </p>
+      </div>
+      <div className='app__chef-sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt='sign' />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
